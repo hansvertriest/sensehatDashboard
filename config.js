994091ea6936bf2d2489d385840283a8b339ccab
hansvertriest <hans.vertriest@gmail.com>
2020-03-24 17:53:36 +0100

    initial

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB-YWpDAU6YIr2QSjXDuskZiH5fo-aV300",
+    authDomain: "pidashboard-866ea.firebaseapp.com",
+    databaseURL: "https://pidashboard-866ea.firebaseio.com",
+    projectId: "pidashboard-866ea",
+    storageBucket: "pidashboard-866ea.appspot.com",
+    messagingSenderId: "1015477576117",
+    appId: "1:1015477576117:web:941bc7d5eabfb8ab89f9fc"
+  };
---
+++ main.js
@@ -0,0 +1,62 @@
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const fs = firebase.firestore();
+
+// LEDs
+const COLLECTION_PIXEL = 'matrix';
+
+const userNameElement = document.getElementById('user-name');
+let userName = undefined;
+
+let color = [0, 0, 0];
+let noColor = [10,0,0];
+
+const registerUser = () => {
+	if (!userName) {
+		userName = userNameElement.value;
+		color = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)]
+		fs.collection(COLLECTION_PIXEL).doc(userName).set({ dummy: true});
+	}
+}
+
+const updatePixel = (event) => {
+	registerUser();
+	if (userName !== undefined) {
+		console.log(45)
+		fs.collection(COLLECTION_PIXEL).doc(userName).update({[event.target.id] : color,});
+	} else {
+		userNameElement.value = prompt("Please give a username");
+		updatePixel();
+	}
+}
+const clearPixels = (event) => {
+	event.preventDefault();
+	for(let box = 0; box < 64; box ++) {
+		const row = Math.floor(box / 8);
+		const column = box % 8;
+		const checkbox = document.getElementById(`pixel-${row}-${column}`);
+		checkbox.checked = false;
+		fs.collection(COLLECTION_PIXEL).doc(userName).delete();
+	}
+}
+
+// data
+
+const updateData = (data) => {
+	const tempElement = document.getElementById("temp");
+	const pressureElement = document.getElementById("pressure");
+	const humidityElement = document.getElementById("humidity");
+	console.log(data);
+	tempElement.innerHTML = data.temp;
+	pressureElement.innerHTML = data.pressure;
+	humidityElement.innerHTML = data.humidity;
+
+}
+
+const COLLECTION_DATA = 'data';
+const DOCUMENT_DATA = 'metrics';
+
+fs.collection(COLLECTION_DATA).doc(DOCUMENT_DATA).onSnapshot((doc) => {
+	updateData(doc.data());
+});
